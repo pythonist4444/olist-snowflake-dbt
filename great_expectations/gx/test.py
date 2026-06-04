c2ee7c89272b6fe1bbd9b8f3cfea27a544063966
pythonist4444 <faccojr0@gmail.com>
2026-06-05 00:23:33 +0100

    feat: Add Great Expectations configuration, expectations and checkpoints for Olist data validation

---
+++ great_expectations/gx/test.py
@@ -0,0 +1,6 @@
+import great_expectations as gx
+
+context = gx.get_context()
+datasource = context.get_datasource("olist_snowflake")
+connector = datasource._data_connectors["default_inferred_data_connector_name"]
+print(connector.get_available_data_asset_names())
